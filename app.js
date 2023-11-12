ffa35a57c9066478d0fce17971285e46ab8ef5fe
Parritz <masong1508@gmail.com>
2023-11-12 15:25:58 -0500

    Switch to Google OAuth2 & clean up code

--- app.js
+++ app.js
@@ -1,75 +1,83 @@
-const jwt = require('jsonwebtoken');
+const fs = require('fs')
 const express = require('express');
 const session = require('express-session');
-const requests = require('requests');
-const fs = require('fs')
-const bodyParser = require('body-parser')
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const { OAuth2Strategy } = require('passport-google-oauth');
+require("dotenv").config();
 
-
-const FBJS_URL = 'http://172.16.3.159:420/';
-const AUTH_URL = 'http://172.16.3.159:420/oauth';
-const THIS_URL = 'http://localhost:3000/login';
+// For this to work, you have to make a .env file with the info from https://console.cloud.google.com/
+// Example: CLIENT_ID=GoogleClientIDHere
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 const PORT = 3069;
-
-// Create an Express App
+if (!fs.existsSync('data.json')) {
+    fs.writeFileSync('data.json', '{}');
+}
 const app = express();
+const data = JSON.parse(fs.readFileSync('data.json'));
 
-// Allow express to use the 'public' folder
-app.use(express.static('public'));
-
-// Read JSON data from HTML POST requests
-app.use(bodyParser.json());
-
-// Use ejs to render html templates in 'views'
-app.set('view engine', 'ejs');
-
-// Store user session data
 app.use(session({
     secret: 'ohnose!',
     resave: false,
     saveUninitialized: false
-}))
+}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
 
-// Load all user data
-var data = JSON.parse(fs.readFileSync('data.json'));
+passport.serializeUser(function(user, callback) {
+    callback(null, user);
+});
+
+passport.deserializeUser(function(obj, callback) {
+    callback(null, obj);
+});
+
+passport.use(new OAuth2Strategy(
+    {
+        clientID: clientID,
+        clientSecret: clientSecret,
+        callbackURL: "http://localhost:3069/login/callback",
+        passReqToCallback: true
+    },
+    function(req, accessToken, refreshToken, profile, done) {
+        return done(null, {
+            profile: profile,
+            username: req.session.requestedUsername
+        });
+    }
+));
 
 // Middleware checks if user is logged in before continuing
 function isAuthenticated(req, res, next) {
-    if (req.session.user) next()
-    else res.redirect(`/login?redirectURL=${THIS_URL}`)
+    if (req.session.user) return next();
+    res.redirect(`/login`);
 };
 
 // Root / homepage
 app.get('/', isAuthenticated, (req, res) => {
-    if (req.session.user) {
-        let userData = {};
-        if (req.session.user in data) {
-            userData = data[req.session.user];
-        }
-        else {
-            data[req.session.user] = { highscore: 0 };
-            userData = data[req.session.user];
-        }
-        res.render('start', { user: userData });
+    let userData = {};
+    if (req.session.user in data) {
+        userData = data[req.session.user];
     } else {
-        res.redirect('/');
+        data[req.session.user] = { highscore: 0 };
+        userData = data[req.session.user];
     }
+    res.render('start', { user: userData });
 });
 
 app.get('/game', isAuthenticated, (req, res) => {
-    if (req.session.user) {
-        let userData = {};
-        if (req.session.user in data) {
-            userData = data[req.session.user];
-        }
-        else {
-            data[req.session.user] = { highscore: 0 };
-            userData = data[req.session.user];
-        }
-        res.render('game', { user: userData });
+    let userData = {};
+    if (req.session.user in data) {
+        userData = data[req.session.user];
     } else {
-        res.redirect('/');
+        data[req.session.user] = { highscore: 0 };
+        userData = data[req.session.user];
     }
+    res.render('game', { user: userData });
 });
 
 app.get('/howplay', isAuthenticated, (req, res) => {
@@ -81,40 +89,54 @@ app.get('/credits', isAuthenticated, (req, res) => {
 });
 
 app.get('/leaderboards', isAuthenticated, (req, res) => {
-    let leaders = [];
-    // Put all players in a list
+    const leaders = [];
     for (const player of Object.keys(data)) {
-        let newObj = {};
-        newObj[player] = data[player].highscore;
-        leaders.push(newObj);
+        const leaderboardData = {};
+        leaderboardData[player] = data[player].highscore;
+        leaders.push(leaderboardData);
     }
-    // Sort by highscore
+
     leaders.sort((a, b) => a.highscore - b.highscore)
     res.render('leaders', { leaders: leaders });
 });
 
 app.post('/submiths', (req, res) => {
     if (req.session.user in data) {
-        if (req.body.score > data[req.session.user].highscore)
+        if (typeof req.body.score === 'number' && req.body.score > data[req.session.user].highscore)
             data[req.session.user] = { highscore: req.body.score };
-    }
-    else {
-        data[req.session.user] = { highscore: req.body.score };
+    } else {
+        if (typeof req.body.score === 'number')
+            data[req.session.user] = { highscore: req.body.score };
     }
     fs.writeFileSync('data.json', JSON.stringify(data));
     res.json({ message: "accepted" });
 });
 
-// Collect login data from Formbar
+// Use Google OAuth2 rather than login data from Formbar
 app.get('/login', (req, res) => {
-    if (req.query.token) {
-        let tokenData = jwt.decode(req.query.token);
-        req.session.token = tokenData;
-        req.session.user = tokenData.username;
-        res.redirect('/');
-    } else {
-        res.redirect(`${AUTH_URL}?redirectURL=${THIS_URL}`);
-    };
+    if (req.session.user) return res.redirect('/');
+    res.render('login');
+});
+
+app.get('/login/auth', (req, res) => {
+    if (req.session.user) return res.redirect('/');
+    if (!req.query.username || req.query.username == "") return res.redirect('/login');
+    req.session.requestedUsername = req.query.username;
+    passport.authenticate('google', { scope : ['email'], passReqToCallback: true })(req, res);
+});
+
+app.get('/login/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+    if (req.session.user) return res.redirect('/');
+
+    const username = req.user?.username;
+    const email = req.user?.profile.emails[0].value;
+    const currentData = data[username];
+    if (currentData && currentData.email && currentData.email != email) return res.redirect('/');
+
+    data[username] = { email: email, highscore: 0 }
+    fs.writeFileSync('data.json', JSON.stringify(data));
+    req.session.user = username;
+    res.redirect('/');
 });
 
 app.listen(PORT, (err) => {
